f103754d1225be8ebdaf40d3fb1070014af39014
anatoliy <anatoly.sakhno@gmail.com>
2021-04-17 13:25:02 +0300

    added test folder

---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,19 @@
+import * as assert from "assert";
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+var vscode = require("vscode");
+
+// import * as myExtension from '../../extension';
+
+suite("Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Start all tests.");
+  after(() => {
+    vscode.window.showInformationMessage("All tests done!");
+  });
+  describe("testessence.gherkin-pdf extension", () => {
+    it("runs", () => {
+      vscode.window.showInformationMessage("test done!");
+    });
+  });
+});
